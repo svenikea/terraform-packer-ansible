51a67192e616e6eab7d829fc520f9e3e9977a84e
svenikea <nguyenhongky1998@gmail.com>
2022-03-30 00:49:33 +0700

    Added EFS Layer

---
+++ terraform/modules/efs_layer/efs.tf
@@ -0,0 +1,15 @@
+resource "aws_efs_file_system" "efs_content" {
+    creation_token              = "${var.project}-efs-content-${var.environment}"
+    encrypted                   = true
+    performance_mode            = var.efs_performance_mode
+    throughput_mode             = var.efs_throughput_mode
+    tags = {
+        Name                    = "${var.project}-efs-content-${var.environment}"
+    }
+}
+
+resource "aws_efs_mount_target" "path" {
+    count                       = length(var.private_subnets)
+    file_system_id              = aws_efs_file_system.efs_content.id
+    subnet_id                   = var.private_subnets[count.index]
+}
---
+++ terraform/modules/efs_layer/variables.tf
@@ -0,0 +1,5 @@
+variable "efs_performance_mode" {}
+variable "efs_throughput_mode" {}
+variable "project" {}
+variable "environment" {}
+variable "private_subnets" {}
