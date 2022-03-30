1605e59cae2b63b17de2f024b67a7e322a949299
svenikea <nguyenhongky1998@gmail.com>
2022-03-30 21:57:23 +0700

    block public policy, acls, restricted public bucket

--- terraform/modules/storage_layer/s3.tf
+++ terraform/modules/storage_layer/s3.tf
@@ -1,15 +1,23 @@
 resource "aws_s3_bucket" "app_bucket" {
-    count               = length(var.bucket_list)
-    bucket              = "${var.project}-${var.bucket_list[count.index]}-${var.environment}-${local.timestamp_filtered}"
-    force_destroy       = true
+    count                   = length(var.bucket_list)
+    bucket                  = "${var.project}-${var.bucket_list[count.index]}-${var.environment}-${local.timestamp_filtered}"
+    force_destroy           = true
     tags = {
-        Name            = "${var.project}-${var.bucket_list[count.index]}-${var.environment}-${local.timestamp_filtered}"
-        Env             = var.environment
+        Name                = "${var.project}-${var.bucket_list[count.index]}-${var.environment}-${local.timestamp_filtered}"
+        Env                 = var.environment
     }
 }
 
+resource "aws_s3_bucket_public_access_block" "block_s3_public_access" {
+    count                   = length(var.bucket_list)
+    bucket                  = aws_s3_bucket.app_bucket.*.id[count.index]
+    block_public_acls       = true
+    block_public_policy     = true
+    restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_acl" "web_static_acl" {
-    count               = length(var.bucket_list)
-    bucket              = aws_s3_bucket.app_bucket.*.id[count.index]
-    acl                 = "private"
+    count                   = length(var.bucket_list)
+    bucket                  = aws_s3_bucket.app_bucket.*.id[count.index]
+    acl                     = "private"
 }
