2f4c25b8546fc76e61dfe63bb58901c5753b832e
svenikea <nguyenhongky1998@gmail.com>
2022-04-15 19:48:39 +0700

    update script and build file

--- packer/scripts/clean.sh
+++ packer/scripts/clean.sh
@@ -4,4 +4,7 @@ source /etc/os-release
 if [[ $ID == "ubuntu" ]]; then
     sudo python3 -m pip uninstall ansible ansible-core -y
     sudo apt remove --autoremove --purge python3-pip git -y
+elif [[ $ID == "amzn" ]]; then
+    sudo python3 -m pip uninstall ansible ansible-core -y
+    sudo yum remove python3-pip telnet git -y
 fi
