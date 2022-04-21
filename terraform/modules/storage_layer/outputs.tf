371bd480c966ee2cbdf4ebe56350aaccb10e7e50
svenikea <nguyenhongky1998@gmail.com>
2022-04-21 10:16:51 +0700

    added s3 lifecycle management

--- terraform/modules/storage_layer/outputs.tf
+++ terraform/modules/storage_layer/outputs.tf
@@ -1,7 +1,7 @@
 output "web_static_domain_name" {
-    value = aws_s3_bucket.app_bucket.*.bucket_domain_name
+    value = aws_s3_bucket.s3_buckets.*.bucket_domain_name
 }
 
 output "bucket_arns" {
-    value = aws_s3_bucket.app_bucket.*.arn
+    value = aws_s3_bucket.s3_buckets.*.arn
 }
--- terraform/modules/storage_layer/s3.tf
+++ terraform/modules/storage_layer/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "app_bucket" {
+resource "aws_s3_bucket" "s3_buckets" {
     count                   = length(var.bucket_list)
     bucket                  = "${var.project}-${var.bucket_list[count.index]}-${var.environment}-${local.timestamp_filtered}"
     force_destroy           = true
@@ -6,11 +6,31 @@ resource "aws_s3_bucket" "app_bucket" {
         Name                = "${var.project}-${var.bucket_list[count.index]}-${var.environment}-${local.timestamp_filtered}"
         Env                 = var.environment
     }
+    versioning {
+        enabled             = true
+    }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "s3_lifecycle" {
+    count                   = length(var.bucket_list)
+    bucket                  = aws_s3_bucket.s3_buckets[count.index].bucket
+    rule {
+        id                  = "${var.project}-${var.bucket_list[count.index]}-${var.environment}-lifecycle"
+        status              = "Enabled"
+        transition {
+            days            = 30
+            storage_class   = "STANDARD_IA"
+        }   
+        transition {
+            days            = 60
+            storage_class   = "GLACIER"
+        }           
+    }
 }
 
 resource "aws_s3_bucket_public_access_block" "block_s3_public_access" {
     count                   = length(var.bucket_list)
-    bucket                  = aws_s3_bucket.app_bucket.*.id[count.index]
+    bucket                  = aws_s3_bucket.s3_buckets.*.id[count.index]
     block_public_acls       = true
     block_public_policy     = true
     restrict_public_buckets = true
@@ -18,6 +38,6 @@ resource "aws_s3_bucket_public_access_block" "block_s3_public_access" {
 
 resource "aws_s3_bucket_acl" "web_static_acl" {
     count                   = length(var.bucket_list)
-    bucket                  = aws_s3_bucket.app_bucket.*.id[count.index]
+    bucket                  = aws_s3_bucket.s3_buckets.*.id[count.index]
     acl                     = "private"
 }
