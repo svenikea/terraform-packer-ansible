c2c81e58e9d5ed825c9ae5c5d21e1c99c4e4130f
svenikea <nguyenhongky1998@gmail.com>
2022-12-28 22:21:52 +0700

    fixed iam user dependencies

--- terraform/modules/iam/iam_groups.tf
+++ terraform/modules/iam/iam_groups.tf
@@ -1,34 +1,25 @@
 resource "aws_iam_group" "group_class" {
-    #for_each    = var.group_name != null ? toset(var.group_name) : []
     count       = var.group_name != null ? 1 : 0
-    #name        = each.value
     name        = var.group_name
     path        = "/users/"
 }
 
 resource "aws_iam_group_membership" "group_memember" {
-    for_each    = var.iam_users != null ? toset(var.iam_users) : []
-    #count       = var.iam_users != null ? length(var.iam_users) : 0
-    name        = "${var.project}-${var.group_name}"
-    #users       = ["${aws_iam_user.iam_users[count.index].name}"]
-    users       = ["${each.value}"]
+    count       = var.iam_users != null ? 1 : 0
+    name        = "${var.project}-${aws_iam_group.group_class[0].name}"
+    users       = aws_iam_user.iam_users.*.name
     group       = aws_iam_group.group_class[0].name
-    #group       = [for group in aws_iam_group.group_class : group.name][0]
-    #group       = [for group in aws_iam_group.group_class : group.name][index(var.iam_users, "${each.value}")]
-    #group       = [for group in aws_iam_group.group_class : group.name][index(var.iam_users, "${each.value}")]
 }
 
-# resource "aws_iam_group_policy_attachment" "attach_group_policies" {
-#     for_each    = var.attach_group_policies != null ? toset(var.attach_group_policis) : []
-#     #count       = var.attach_group_policies != null ? length(var.attach_group_policies) : 0
-#     group       = [for group in aws_iam_group.group_class : group.name][index(var.attach_group_policis, "${each.value}")]
-#     policy_arn  = each.value
-# }
+resource "aws_iam_group_policy_attachment" "attach_group_policies" {
+    count       = var.attach_group_policies != null ? length(var.attach_group_policies) : 0
+    group       = aws_iam_group.group_class[0].name
+    policy_arn  = var.attach_group_policies[count.index]
+}
 
-# resource "aws_iam_group_policy" "inline_group_policy" {
-#     for_each    = var.inline_group_policies != null ? toset(var.inline_group_policies)  : []
-#     #count       = var.inline_group_policies != null ? 1 : 0
-#     group       = [for group in aws_iam_group.group_class : group.name][index(var.inline_group_policies, "${each.value}")]
-#     policy      = each.value
-# }
+resource "aws_iam_group_policy" "inline_group_policy" {
+    count       = var.inline_group_policies != null ? 1 : 0
+    group       = aws_iam_group.group_class[0].name
+    policy      = var.inline_group_policies
+}
 
--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -5,6 +5,6 @@ output "service_role" {
     value = var.new_roles != false ? aws_iam_role.iam_role[0].arn : null
 }
 
-output "iam_users" {
-    value = var.iam_users != null ? [for iam_user in aws_iam_user.iam_users : iam_user.name] : null
+output "iam_user" {
+    value = aws_iam_user.iam_users.*.name
 }
