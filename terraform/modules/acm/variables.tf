b38b557cf78cce1a483e5c4e34b48cdfc91e433c
svenikea <nguyenhongky1998@gmail.com>
2022-12-30 17:08:19 +0700

    added missing variable

--- terraform/modules/acm/variables.tf
+++ terraform/modules/acm/variables.tf
@@ -5,6 +5,5 @@ variable "private_key" { default = null }
 variable "certificate_body" { default = null }
 
 ## If using Route 53 then use these resources else comment it
-variable "zone_id" { default = null }
 variable "route53_enable" { default = null }
 variable "new_acm" { default = null }
